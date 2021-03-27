9325b171603bdb200175d670f38357f0289c1a3b
murariugabriela <79075813+murariugabriela@users.noreply.github.com>
2021-03-27 21:05:07 +0200

    adaugare functie citat random

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -5,5 +5,8 @@ var i=0;
       else
         document.getElementById("myForm").style.display = "block";
       i=i+1;
-  
+  }
+  function citat(){
+    var citate = ["I’ve always enjoyed mathematics. It is the most precise and concise way of expressing an idea.","There should be no such thing as boring mathematics.","Mathematics is the music of reason.","Matematician nu este cel ce știe matematică, ci cel ce creează matematică.","Matematica este muzica raţiunii.","Lumea este condusă de numere.","Pure mathematics is, in its way, the poetry of logical ideas.","Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers."];
+    document.getElementById("citat").innerHTML = citate[Math.floor(Math.random() * (citate.length - 1))];
   }
