7d4f013d109e7ff0950c57a15275c767f35aa058
murariugabriela <79075813+murariugabriela@users.noreply.github.com>
2021-04-11 20:04:55 +0300

    adaugare functie countdown test fulger

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -33,45 +33,64 @@ function displayCenterContentItem(id) {
   document.getElementById(id).style.flexDirection = "column";
 }
 function verifyAnswer(prefix) {
-  document.getElementById(prefix+'input1').textContent = '';
-  document.getElementById(prefix+'input2').textContent = '';
-  document.getElementById(prefix+'input3').textContent = '';
-  document.getElementById(prefix+'input4').textContent = '';
-  document.getElementById(prefix+'b').style.border = '1px solid gray';
-  document.getElementById(prefix+'d').style.border = '1px solid gray';
-  document.getElementById(prefix+'a').style.border = '1px solid gray';
-  document.getElementById(prefix+'c').style.border = '1px solid gray';
-  if (document.getElementById(prefix+'var1').checked) {
-    if (document.getElementById(prefix+'var1').value == 'true')
-      document.getElementById(prefix+'a').style.border = '1px solid limegreen';
+  document.getElementById(prefix + 'input1').textContent = '';
+  document.getElementById(prefix + 'input2').textContent = '';
+  document.getElementById(prefix + 'input3').textContent = '';
+  document.getElementById(prefix + 'input4').textContent = '';
+  document.getElementById(prefix + 'b').style.border = '1px solid gray';
+  document.getElementById(prefix + 'd').style.border = '1px solid gray';
+  document.getElementById(prefix + 'a').style.border = '1px solid gray';
+  document.getElementById(prefix + 'c').style.border = '1px solid gray';
+  if (document.getElementById(prefix + 'var1').checked) {
+    if (document.getElementById(prefix + 'var1').value == 'true')
+      document.getElementById(prefix + 'a').style.border = '3px solid limegreen';
     else
-      document.getElementById(prefix+'a').style.border = '1px solid red';
+      document.getElementById(prefix + 'a').style.border = '3px solid red';
   }
-  else if (!document.getElementById(prefix+'var1').checked && document.getElementById(prefix+'var1').value == 'true')
-    document.getElementById(prefix+'input1').textContent = 'Trebuia bifat';
-  if (document.getElementById(prefix+'var2').checked) {
-    if (document.getElementById(prefix+'var2').value == 'true')
-      document.getElementById(prefix+'b').style.border = '1px solid limegreen';
+  else if (!document.getElementById(prefix + 'var1').checked && document.getElementById(prefix + 'var1').value == 'true')
+    document.getElementById(prefix + 'input1').textContent = 'Trebuia bifat';
+  if (document.getElementById(prefix + 'var2').checked) {
+    if (document.getElementById(prefix + 'var2').value == 'true')
+      document.getElementById(prefix + 'b').style.border = '3px solid limegreen';
     else
-      document.getElementById(prefix+'b').style.border = '1px solid red';
+      document.getElementById(prefix + 'b').style.border = '3px solid red';
   }
-  else if (!document.getElementById(prefix+'var2').checked && document.getElementById(prefix+'var2').value == 'true')
-    document.getElementById(prefix+'input2').textContent = 'Trebuia bifat';
-  if (document.getElementById(prefix+'var3').checked) {
-    if (document.getElementById(prefix+'var3').value == 'true')
-      document.getElementById(prefix+'c').style.border = '1px solid limegreen';
+  else if (!document.getElementById(prefix + 'var2').checked && document.getElementById(prefix + 'var2').value == 'true')
+    document.getElementById(prefix + 'input2').textContent = 'Trebuia bifat';
+  if (document.getElementById(prefix + 'var3').checked) {
+    if (document.getElementById(prefix + 'var3').value == 'true')
+      document.getElementById(prefix + 'c').style.border = '3px solid limegreen';
     else
-      document.getElementById(prefix+'c').style.border = '1px solid red';
+      document.getElementById(prefix + 'c').style.border = '3px solid red';
   }
-  else if (!document.getElementById(prefix+'var3').checked && document.getElementById(prefix+'var3').value == 'true')
-    document.getElementById(prefix+'input3').textContent = 'Trebuia bifat';
-  if (document.getElementById(prefix+'var4').checked) {
-    if (document.getElementById(prefix+'var4').value == 'true')
-      document.getElementById(prefix+'d').style.border = '1px solid limegreen';
+  else if (!document.getElementById(prefix + 'var3').checked && document.getElementById(prefix + 'var3').value == 'true')
+    document.getElementById(prefix + 'input3').textContent = 'Trebuia bifat';
+  if (document.getElementById(prefix + 'var4').checked) {
+    if (document.getElementById(prefix + 'var4').value == 'true')
+      document.getElementById(prefix + 'd').style.border = '3px solid limegreen';
     else
-      document.getElementById(prefix+'d').style.border = '1px solid red';
+      document.getElementById(prefix + 'd').style.border = '3px solid red';
   }
-  else if (!document.getElementById(prefix+'var4').checked && document.getElementById(prefix+'var4').value == 'true')
-    document.getElementById(prefix+'input4').textContent = 'Trebuia bifat';
+  else if (!document.getElementById(prefix + 'var4').checked && document.getElementById(prefix + 'var4').value == 'true')
+    document.getElementById(prefix + 'input4').textContent = 'Trebuia bifat';
 
 }
+var seconds = 150;
+var sufixCurent;
+function displayTimer(sufix) {
+  var id = 'timer';
+  sufixCurent = sufix;
+  var sufixNou;
+  document.getElementById(id + sufixCurent).innerHTML = seconds.toString();
+  document.getElementById('divtimer' + sufixCurent).style.background = 'yellow';
+  seconds--;
+  if(seconds == -1)
+  {
+    document.getElementById(id + sufixCurent).innerHTML = '';
+    document.getElementById('divtimer' + sufixCurent).style.background = 'red';
+    sufixNou = parseInt(sufix) + 1;
+    sufixCurent =  sufixNou.toString();
+    seconds = 150;
+  }
+}
+var interval = setInterval(function() { displayTimer(sufixCurent); }, 1000);
