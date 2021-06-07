19f152a9000d7dd409d786a98c55e397855a0516
Bordeianu-Cocea Adelina-Mihaela <79091943+adelinabordeianu@users.noreply.github.com>
2021-06-07 20:30:29 +0300

    Adelina+Gabi+Alex

---
+++ php/download.php
@@ -0,0 +1,17 @@
+<?php
+
+$filename = '../csv/persons.csv';
+header('Pragma: public');
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Cache-Control: private', false); 
+header('Content-Type: application/pdf');
+
+header('Content-Disposition: attachment; filename="'. basename($filename) . '";');
+header('Content-Transfer-Encoding: binary');
+header('Content-Length: ' . filesize($filename));
+
+readfile($filename);
+
+exit;
+?>
